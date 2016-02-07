cb3f884afe61a4d3826b858855aaf82e579a5231
aubonpain <abi.sampath@gmail.com>
2016-02-07 18:24:54 -0500

    Got the db sending data to goals. Now have to work on wiring Users with Goals (and Goals with SubGoals)

--- templates/goals.js
+++ templates/goals.js
@@ -3,29 +3,7 @@ Goals = new Meteor.Collection('goals');
 if (Meteor.isClient) {
     Template.goals.helpers({
         goals: function () {
-            return [
-                {
-                    goal: "goal1",
-                    subGoals: [
-                        {subGoal: "subGoal1_1"},
-                        {subGoal: "subGoal1_2"}
-                    ]
-                },
-                {
-                    goal: "goal2",
-                    subGoals: [
-                        {subGoal: "subGoal2_1"},
-                        {subGoal: "subGoal2_2"}
-                    ]
-                },
-                {
-                    goal: "goal3",
-                    subGoals: [
-                        {subGoal: "subGoal3_1"},
-                        {subGoal: "subGoal3_2"}
-                    ]
-                }
-            ];
+            return Goals.find();
         }
     });
 }
