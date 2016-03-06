1e61514440e802fe9bdd2462c8e9cbbed6ff6873
aubonpain <abi.sampath@gmail.com>
2016-03-06 08:53:50 -0500

    Refactoring work, added editing for main goals, some CSS for Measurements

--- templates/goalList.js
+++ templates/goalList.js
@@ -15,6 +15,46 @@ Meteor.methods({
         Goals.remove({
             _id: goalId
         });
+    },
+
+    editGoal: function (id) {
+        // We want to make the goal editable
+        // We want to have a Save button
+        // We want to have a Cancel button
+        Goals.update(id,
+            {
+                $set: { beingEdited: true }
+            }
+        )
+    },
+
+    cancelEditGoal: function (id) {
+        // We want to make the goal editable
+        // We want to have a Save button
+        // We want to have a Cancel button
+        Goals.update(id,
+            {
+                $set: { beingEdited: false }
+            }
+        )
+    },
+
+    saveEditGoal: function (id, goal, goalStartDate, goalDeadline, description) {
+        // We want to make the goal editable
+        // We want to have a Save button
+        // We want to have a Cancel button
+        Goals.update(
+            id,
+            {
+                $set: {
+                    goal: goal,
+                    goalStartDate: goalStartDate,
+                    goalEndDate: goalDeadline,
+                    description: description,
+                    beingEdited: false
+                }
+            }
+        );
     }
 
 });
@@ -39,6 +79,25 @@ if (Meteor.isClient) {
             e.preventDefault();
 
             Meteor.call("deleteGoal", this._id);
+        },
+
+        "click #editGoal": function (e) {
+            e.preventDefault();
+
+            Meteor.call("editGoal", this._id);
+        },
+
+        "click #cancelEditGoal": function (e) {
+            Meteor.call("cancelEditGoal", this._id);
+        },
+
+        "click #saveEditGoal": function (e, template) {
+            Meteor.call("saveEditGoal",
+                this._id,
+                template.find("#goal" + this._id).value,
+                template.find("#goalStart" + this._id).value,
+                template.find("#goalEnd" + this._id).value,
+                template.find("#goalDescription" + this._id).value);
         }
     });
 }
